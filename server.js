6de6f90fb7d94ea5fefc8146d16847af2095455a
Anant Mathur <anant.mathur007@gmail.com>
2020-07-26 16:36:46 +0530

    prepare for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 
 // Middleware utilities
 const mongoSanitize = require("express-mongo-sanitize");
@@ -7,51 +8,57 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const bodyParser = require("body-parser");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-// Mongoose 
-const mongoose = require('mongoose');
+// Mongoose
+const mongoose = require("mongoose");
 
 // Config variables
-require('dotenv').config();
-
+require("dotenv").config();
 
 //Connecting to the database
 mongoose.promise = global.Promise;
-mongoose.connect(process.env.MongoURI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true}, (err,db)=> {
-    if(err)
-    console.log(err);
-
-    else
-    console.log('Database Connected...');
-});
+mongoose.connect(
+  process.env.MongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (err, db) => {
+    if (err) console.log(err);
+    else console.log("Database Connected...");
+  }
+);
 
 // Getting data in json format
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // API SECURITY
-app.use(mongoSanitize());   // Sanitize Data
-app.use(helmet());          // Set security headers
-app.use(xss())              // Prevent XSS(cross site scripting) attacks
-app.use(hpp());             // Prevent hpp param pollution
+app.use(mongoSanitize()); // Sanitize Data
+app.use(helmet()); // Set security headers
+app.use(xss()); // Prevent XSS(cross site scripting) attacks
+app.use(hpp()); // Prevent hpp param pollution
 
 // Dev Middleware
-app.use(morgan('dev'));
-
-// Test route
-app.get("/api", (req, res) => {
-    res.json({
-        success: true,
-        message: "API running"
-    });
-})
+app.use(morgan("dev"));
 
 // Mounting the routes
 app.use("/api/adminUser/auth", require("./routes/adminUserAuth"));
 app.use("/api/adminUser", require("./routes/adminUser"));
 
+// Serve Static Assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Starting the server
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log(`Server started on port ${process.env.PORT || 5000}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server started on port ${process.env.PORT || 5000}`);
 });
